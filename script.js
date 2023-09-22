d102c8e2215953f16651ac5d82fb0b11574069f0
Abhik Baidya <abhikbaidya7@gmail.com>
2023-09-22 11:24:51 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+var main = document.querySelector(".main")
+var curs = document.querySelector(".cursor")
+
+main.addEventListener("mousemove", function(dets){
+curs.style.left = dets.x+"px"
+curs.style.top = dets.y+"px"
+})
+main.addEventListener("touchmove", function(dets){
+    curs.style.left = dets.x+"px"
+    curs.style.top = dets.y+"px"
+    })
